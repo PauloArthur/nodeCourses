dff6e64be44e3c730c46f78ca792cfe9271181a9
PauloArthur <pauloarthurcostamendes@gmail.com>
2023-01-21 20:37:09 -0300

    reading and writing fichas data on json file

--- mefit/database/fichas.js
+++ mefit/database/fichas.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 
 const makeFichas = () => [
     makeFicha('A', 'Peito', 'Aqui eh so peitola', 8),
@@ -21,4 +25,31 @@ const getRandomIntBetweenMinAndMax = (min = 0, max = Number.MAX_SAFE_INTEGER) =>
     return min + Math.floor(Math.random() * (max - min));
 }
 
-module.exports = makeFichas
+const readFichasFile = async () => {
+    try {
+        const data = await readFile('./database/fichas.json', 'utf8')
+        return data ? JSON.parse(data) : data
+    } catch (err) {
+        console.log('Something went wrong reading fichas.json file')
+        console.log('Error:', err)
+    }
+}
+
+const writeFichasFile = async (fichas) => {
+    await writeFile('./database/fichas.json', JSON.stringify(fichas))
+}
+
+const getFichas = async () => {
+    let fichas = await readFichasFile()
+    if (fichas === '') {
+        console.log('fichas is empty')
+        fichas = makeFichas()
+        await writeFichasFile(fichas)
+    } else {
+        console.log('fichas not empty')
+    }
+
+    return fichas
+}
+
+module.exports = getFichas
--- mefit/index.js
+++ mefit/index.js
@@ -1,21 +1,24 @@
-const makeFichas = require('./database/fichas')
+const getFichas = require('./database/fichas')
 const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-const fichas = makeFichas()
+async function main (){
+    const fichas = await getFichas()
+    
+    app.get('/', (req, res) => {
+        res.render('index', { fichas })
+    })
+    
+    app.get('/ficha/:id', (req, res) => {
+        const title = 'MeFit';
+        const id = req.params.id;
+        res.render('ficha/index', { title: title, ficha: fichas[id] })
+    })
+    
+    app.listen(8080, () => console.log('App running...'))
+}
 
-app.get('/', (req, res) => {
-    const title = 'MeFit';
-    res.render('index', { title: title, fichas })
-})
-
-app.get('/ficha/:id', (req, res) => {
-    const title = 'MeFit';
-    const id = req.params.id;
-    res.render('ficha/index', { title: title, ficha: fichas[id] })
-})
-
-app.listen(8080, () => console.log('App running'))
+main()
